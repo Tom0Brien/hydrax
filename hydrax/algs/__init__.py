3c1d57e639ac60c79b337fb0a130db0c83e1f63c
Tom0Brien <thomas16441@gmail.com>
2025-12-18 18:14:36 +1100

    Fix algs init

---
+++ hydrax/algs/__init__.py
@@ -0,0 +1,7 @@
+from .cem import CEM
+from .evosax import Evosax
+from .mppi import MPPI
+from .predictive_sampling import PredictiveSampling
+from .dial import DIAL
+from .icem import iCEM
+__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax", "DIAL", "iCEM"]
